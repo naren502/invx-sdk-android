9f477c78ca027863258c0ca138350c3e26d6b28b
Anthony Ashbrook <anthony.ashbrook@gmail.com>
2015-02-19 00:30:44 +0000

    Initial project with working camera

---
+++ app/src/main/java/com/machineswithvision/vxview/CameraActivity.java
@@ -0,0 +1,286 @@
+package com.machineswithvision.vxview;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+import java.io.IOException;
+
+public class CameraActivity extends Activity {
+    private static final String TAG = "CameraActivity";
+
+    // State
+    private boolean weHaveBeenResumed = false;
+    private boolean existingSurfaceHasSize = false;
+    private boolean cameraIsActive = false;
+    private int preWidth;
+    private int preHeight;
+
+    // Components
+    private SurfaceHolder holder;
+    private Camera camera;
+
+    private boolean focusAuto = false;
+    private boolean focused = true;
+
+    private Object contAutoFocusCallback = null;
+
+    private static final String FOCUS_CONTINUOUS_AUTO = "continuous-picture";
+
+    /**
+     *
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a fixed-orientation fullscreen view with no title
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_camera);
+
+        final SurfaceView surface = (SurfaceView)findViewById(R.id.surface_preview);
+        holder = surface.getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            /**
+             *
+             */
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                synchronized (CameraActivity.this) {
+                    if (weHaveBeenResumed) {
+                        createCamera();
+                        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "Surface created - creating camera");
+                    } else {
+                        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "Surface created - not yet resumed so don't create the camera");
+                    }
+                }
+            }
+
+            /**
+             *
+             */
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "Surface changed: "+format+","+width+","+height);
+                synchronized (CameraActivity.this) {
+                    if (cameraIsActive) {
+                        if (Log.isLoggable(TAG, Log.WARN)) Log.w(TAG, "Scanner is already active - ignoring!");
+                    } else {
+                        // We always expect the reader screen to run fixed in landscape mode.
+                        // In some circs the moto milestone running 2.1 reverses the width/height
+                        if (width<height) {
+                            if (Log.isLoggable(TAG, Log.WARN)) Log.w(TAG, "Width and height appear to be swapped - swapping back!");
+                            int tmp = width;
+                            width = height;
+                            height = tmp;
+                        }
+
+                        preWidth = width;
+                        preHeight = height;
+
+                        if (weHaveBeenResumed) {
+                            startCamera();
+                        } else {
+                            if (Log.isLoggable(TAG, Log.DEBUG))Log.d(TAG, "Not yet resumed so don't start the camera yet!");
+                        }
+
+                        existingSurfaceHasSize = true;
+                    }
+                }
+            }
+
+            /**
+             *
+             */
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                if (Log.isLoggable(TAG, Log.DEBUG))Log.d(TAG, "Surface destroyed");
+                existingSurfaceHasSize = false;
+
+                if (cameraIsActive) {
+                    if (Log.isLoggable(TAG, Log.WARN)) Log.w(TAG, "We should never get here with an active camera!");
+                }
+            }
+        });
+    }
+
+    /**
+     * Called after onRestoreInstanceState(Bundle), onRestart(), or onPause(), for your activity to start interacting with the user.
+     * This is a good place to begin animations, open exclusive-access devices (such as the camera), etc.
+     */
+    @Override
+    protected void onResume() {
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "onResume()");
+        super.onResume();
+
+        synchronized (this) {
+            weHaveBeenResumed = true;
+
+            if (existingSurfaceHasSize && !cameraIsActive) {
+                if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "Surface already exists so create and start the camera.");
+                createCamera();
+                startCamera();
+            } else {
+                if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "No surface yet so don't create the camera.");
+            }
+        }
+    }
+
+    /**
+     * Called as part of the activity lifecycle when an activity is going into the background, but has not (yet) been killed. The counterpart to onResume().
+     */
+    @Override
+    protected void onPause() {
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "onPause()");
+        super.onPause();
+
+        // Stop camera - synchronized to avoid races
+        synchronized (this) {
+            weHaveBeenResumed = false;
+            stopAndReleaseCamera();
+        }
+    }
+
+    /**
+     *
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_camera, menu);
+        return true;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     *
+     */
+    private void createCamera() {
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "createCamera()");
+
+        camera = Camera.open();
+
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to set camera preview surface!", e);
+        }
+    }
+
+    /**
+     *
+     */
+    private void startCamera() {
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "startScanner()");
+
+        Camera.Parameters parameters = camera.getParameters();
+
+        //Attempt to set up camera with VGA preview frame output or as close as possible
+        ResolutionOptions preOptions = new ResolutionOptions(parameters.get("preview-size-values"));
+        if (preOptions.getSize()>0) {
+            ResolutionOptions.Option selected = preOptions.leastDifference(640, 480);
+            preWidth = selected.getWidth();
+            preHeight = selected.getHeight();
+            Log.d(TAG, "Preview setup as "+preWidth+"x"+preHeight);
+        } else {
+            // Make sure we are multiples of 8
+            preWidth = (preWidth>>3)<<3;
+            preHeight = (preHeight>>3)<<3;
+        }
+        parameters.setPreviewSize(preWidth, preHeight);
+
+        parameters.setFocusMode(FOCUS_CONTINUOUS_AUTO);
+
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "Preview setup as "+preWidth+"x"+preHeight);
+
+        try{
+            camera.setParameters(parameters);
+        }catch(Exception e) {
+            //continuous autofocus not supported, set macro instead
+            focusAuto = true;
+            try{
+                parameters.setFocusMode(Parameters.FOCUS_MODE_MACRO);
+                camera.setParameters(parameters);
+            }catch(Exception e2) {
+                //macro not supported either, use normal autofocus triggered by user's tap
+                parameters.setFocusMode(Parameters.FOCUS_MODE_AUTO);
+                camera.setParameters(parameters);
+            }
+        }
+        camera.startPreview();
+        cameraIsActive=true;
+
+        //this doesn't exist in older APIs, but we only use it on newer APIs
+        //Set 'focused' boolean to indicate whether we are in the middle of a continuous autofocus cycle
+        //If we are in the middle of a cycle, image is likely to be unfocused - therefore we wait until the end of the cycle
+        if(!focusAuto) {
+            contAutoFocusCallback = new Camera.AutoFocusMoveCallback() {
+
+                @Override
+                public void onAutoFocusMoving(boolean start, Camera camera) {
+                    if(start) {
+                        focused = false;
+                    }else{
+                        focused = true;
+                    }
+                }
+            };
+            camera.setAutoFocusMoveCallback((Camera.AutoFocusMoveCallback) contAutoFocusCallback);
+        }
+
+        camera.setPreviewCallback(previewCallback);
+    }
+
+    /**
+     *
+     */
+    private void stopAndReleaseCamera() {
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "stopScannerReleaseCamera()");
+
+        cameraIsActive=false;
+        if (camera!=null) {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera=null;
+        } else {
+            if (Log.isLoggable(TAG, Log.WARN)) Log.w(TAG, "No camera to stop - can't have been built yet.");
+        }
+    }
+
+    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/machineswithvision/vxview/ResolutionOptions.java
@@ -0,0 +1,94 @@
+package com.machineswithvision.vxview;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import android.util.Log;
+/**
+ * Created by Anthony on 19/02/2015.
+ */
+public class ResolutionOptions {
+    // Too see all logging use "adb shell setprop log.tag.ResolutionOptions DEBUG" - default is WARN and above
+    private static final String TAG = "ResolutionOptions";
+
+    private static final Pattern COMMA = Pattern.compile(",");
+
+    private Option[] options;
+
+    public ResolutionOptions(String resolutionList) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) Log.d(TAG, "Initialised with: "+resolutionList);
+
+        if (resolutionList==null) {
+            options = new Option[0];
+        } else {
+            List<Option> temp = new LinkedList<Option>();
+
+            for (String size : COMMA.split(resolutionList)) {
+                size = size.trim();
+
+                int xPos = size.indexOf('x');
+                int width = Integer.parseInt(size.substring(0, xPos));
+                int height = Integer.parseInt(size.substring(xPos+1));
+
+                temp.add(new Option(width,height));
+            }
+
+            if (temp.size()>=2 && temp.get(0).getWidth()<temp.get(temp.size()-1).getWidth()) {
+                Collections.reverse(temp);
+            }
+
+            options = temp.toArray(new Option[temp.size()]);
+        }
+    }
+
+    public int getSize() {
+        return options.length;
+    }
+
+    public Option biggestWithin(int width, int height) {
+        Option result = null;
+
+        for (int i=0; i<options.length && result==null; i+=1) {
+            if (options[i].getWidth()<=width && options[i].getHeight()<=height) {
+                result = options[i];
+            }
+        }
+
+        return result;
+    }
+
+    public Option leastDifference(int width, int height) {
+        Option result = null;
+        int bestDiff = Integer.MAX_VALUE;
+
+        for (int i=0; i<options.length; i+=1) {
+            int newDiff = Math.abs(options[i].getWidth()-width)+Math.abs(options[i].getHeight()-height);
+            if (newDiff<bestDiff) {
+                bestDiff=newDiff;
+                result = options[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static class Option {
+        private int width;
+        private int height;
+
+        public Option(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+    }
+}
