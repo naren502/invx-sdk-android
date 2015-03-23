6b85713b0ee59525dadb27bd5e3accb8fb4cba1a
Anthony Ashbrook <anthony.ashbrook@gmail.com>
2015-03-23 22:28:54 +0000

    Updated path to invx library

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := openvx
-LOCAL_SRC_FILES := $(LOCAL_PATH)/openvx/$(TARGET_ARCH_ABI)/libopenvx.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/invx/lib/$(TARGET_ARCH_ABI)/libopenvx.so
 $(warning $(TARGET_ARCH_ABI))
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -10,7 +10,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vxu
-LOCAL_SRC_FILES := $(LOCAL_PATH)/openvx/$(TARGET_ARCH_ABI)/libvxu.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/invx/lib/$(TARGET_ARCH_ABI)/libvxu.so
 $(warning $(TARGET_ARCH_ABI))
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -18,7 +18,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := openvx-c_model
-LOCAL_SRC_FILES := $(LOCAL_PATH)/openvx/$(TARGET_ARCH_ABI)/libopenvx-c_model.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/invx/lib/$(TARGET_ARCH_ABI)/libopenvx-c_model.so
 $(warning $(TARGET_ARCH_ABI))
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -26,7 +26,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := openvx-debug
-LOCAL_SRC_FILES := $(LOCAL_PATH)/openvx/$(TARGET_ARCH_ABI)/libopenvx-debug.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/invx/lib/$(TARGET_ARCH_ABI)/libopenvx-debug.so
 $(warning $(TARGET_ARCH_ABI))
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -34,7 +34,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := openvx-extras
-LOCAL_SRC_FILES := $(LOCAL_PATH)/openvx/$(TARGET_ARCH_ABI)/libopenvx-extras.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/invx/lib/$(TARGET_ARCH_ABI)/libopenvx-extras.so
 $(warning $(TARGET_ARCH_ABI))
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -46,7 +46,7 @@ LOCAL_MODULE := NDK
 LOCAL_SRC_FILES := \
      com_machineswithvision_openvx_JOVX.c
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/openvx/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/invx/include
 LOCAL_SHARED_LIBRARIES := openvx vxu openvx-extras openvx-debug openvx-c_model
 LOCAL_LDLIBS += -llog
 
